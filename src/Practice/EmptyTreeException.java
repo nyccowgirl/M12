b89a20252b669d22c974d87f3273300c43f2b357
nyccowgirl <nyc_cowgirl@hotmail.com>
2021-04-22 21:45:54 -0500

    Initial commit

---
+++ src/Practice/EmptyTreeException.java
@@ -0,0 +1,19 @@
+package Practice;
+
+/**
+ * A class of runtime exceptions thrown by methods to indicate that a tree is
+ * empty.
+ * 
+ * @author Frank M. Carrano
+ * @author Timothy M. Henry
+ * @version 5.0
+ */
+public class EmptyTreeException extends RuntimeException {
+	public EmptyTreeException() {
+		this(null);
+	} // end default constructor
+
+	public EmptyTreeException(String message) {
+		super(message);
+	} // end constructor
+} // end Practice.EmptyTreeException
